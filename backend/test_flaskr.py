80fd4f9dac00c2a923a82738c4f21fc2f0f7054d
Rick Morrow <rclark.morrow@gmail.com>
2020-05-03 13:57:21 -0700

    Setup test DB, initial unittest
    Setup test database and added database username and password to
    test_flaskr.py config.
    
    Created initial test for getting the list of trivia categories.
    
    TODO: Setup route to get list of categories, test response, test
    frontend.

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,3 +1,7 @@
+""" ---------------------------------------------------------------------------
+# Imports
+# --------------------------------------------------------------------------"""
+
 import os
 import unittest
 import json
@@ -7,6 +11,11 @@ from flaskr import create_app
 from models import setup_db, Question, Category
 
 
+""" ---------------------------------------------------------------------------
+# Tests
+# --------------------------------------------------------------------------"""
+
+
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
 
@@ -14,8 +23,12 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
+        self.db_user = 'postgres'
+        self.db_passw = 'postgres'
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = ("postgres://{}:{}@{}/{}"
+                              .format(self.db_user, self.db_passw,
+                                      'localhost:5432', self.database_name))
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,14 +37,27 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
+    def getTest():
+        print(test)
+
     def tearDown(self):
         """Executed after reach test"""
         pass
 
+    def test_get_categories(self):
+        """ Tests category list response"""
+        response = self.client().get('/categories')
+        data = json.loads(response.data)
+
+        self.assertEqual(ressponse.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['categories']))
+
     """
     TODO
-    Write at least one test for each test for successful operation and for expected errors.
+    Write at least one test for each test for successful
+    operation and for expected errors.
     """
 
 
